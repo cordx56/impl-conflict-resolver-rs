dbf45bcea2c2fab4fae00f37e1a06e697b573062
CHISEN Kaoru <cordx56@cordx.cx>
2024-01-05 13:12:13 +0900

    サンプル修正

--- sample6.rs
+++ sample6.rs
@@ -3,16 +3,8 @@ trait TA {}
 trait TB {}
 trait TC: TA + TB {}
 trait TD {}
-trait TE {}
-trait TF {}
 trait TP<P> {}
-
 impl<P: TA> TP<P> for A {}
 impl<P: TA + TB> TP<P> for A {}
 impl<P: TC> TP<P> for A {}
 impl<P: TD - TA> TP<P> for A {}
-impl<P: TE + TA - TB> TP<P> for A {}
-impl<P: TA, Q: TP<P>> TP<Q> for A {}
-impl<P: TB - TA, Q: TP<P>> TP<Q> for A {}
-impl TF for A {}
-impl TF for A {}
